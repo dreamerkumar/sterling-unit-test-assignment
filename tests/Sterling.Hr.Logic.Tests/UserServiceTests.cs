11ba4f28ac0c3bbc90aef8dda62d75fff94d4b1d
Vishal Kumar <connectwithvkumar@gmail.com>
2016-06-26 18:45:45 -0400

    Adding tests for User Service

--- tests/Sterling.Hr.Logic.Tests/UserServiceTests.cs
+++ tests/Sterling.Hr.Logic.Tests/UserServiceTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Sterling.Hr.Logic;
+using Rhino.Mocks;
 
 namespace Sterling.Hr.Logic.Tests
 {
@@ -13,12 +14,40 @@ namespace Sterling.Hr.Logic.Tests
         public void Successfully_Returns_All_Users_When_Passing_In_False()
         {
             // unit test goes here
+            var allUsers = new User[] { new User(), new User(), new User(), new User() };
+
+            //mock objects
+            var stubUserDataAccess = MockRepository.GenerateStub<IUserDataAccess>();
+            stubUserDataAccess.Stub(x => x.GetAllUsers()).Return(allUsers);
+
+            //run tests
+            concern = new UserService(stubUserDataAccess);
+            var result = concern.GetUsers(false);
+
+            //assert
+            stubUserDataAccess.AssertWasCalled(x => x.GetAllUsers());
+            Assert.IsNotNull(result);
+            Assert.AreEqual(result.Length, 4);
         }
 
         [TestMethod]
         public void Successfully_Returns_All_Active_Users_When_Passing_In_True()
         {
             // unit test goes here
+            var activeUsers = new User[] { new User(), new User() };
+
+            //mock objects
+            var stubUserDataAccess = MockRepository.GenerateStub<IUserDataAccess>();
+            stubUserDataAccess.Stub(x => x.GetAllActiveUsers()).Return(activeUsers);
+
+            //run tests
+            concern = new UserService(stubUserDataAccess);
+            var result = concern.GetUsers(true);
+
+            //assert
+            stubUserDataAccess.AssertWasCalled(x => x.GetAllActiveUsers());
+            Assert.IsNotNull(result);
+            Assert.AreEqual(result.Length, 2);
         }
     }
 }
